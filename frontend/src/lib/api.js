c2c7de88f1aab232089942f6605f33fb9a71608e
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-11-06 14:54:10 +0530

    add localhost

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080";
 
 export const api = axios.create({
   baseURL: baseURL,
