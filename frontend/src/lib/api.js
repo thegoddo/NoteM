51c5cf37eb08a68c984d0fb11a75233dd37c7f96
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-11-06 14:47:47 +0530

    remove localhost

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080";
+const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const api = axios.create({
   baseURL: baseURL,
